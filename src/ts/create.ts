0588d175938bc7b42a51f48f137f1d4b72a272d3
muzaffarovhayotbek <hayotbek0511@gmail.com>
2025-03-04 18:13:56 +0500

    end

--- src/ts/create.ts
+++ src/ts/create.ts
@@ -1,1 +1,15 @@
+import { PizzaModel } from './model/Pizza';
 
+const form = document.querySelector('form') as HTMLFormElement;
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const formEl = new FormData(form);
+
+  const newPizza: PizzaModel = {
+    title: formEl.get('title') as string,
+    description: formEl.get('description') as string,
+    toppings: formEl.getAll('toppings') as string[],
+    price: parseInt(formEl.get('price') as string),
+  };
+ await newPizza.save();
+});
