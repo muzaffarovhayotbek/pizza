d54b687f3f93cb006ef1dfe60159cd721d54a58f
muzaffarovhayotbek <hayotbek0511@gmail.com>
2025-03-04 17:14:49 +0500

    main

---
+++ src/ts/model/Pizza.ts
@@ -0,0 +1,12 @@
+import { DataStructure } from '../services/DataStructure';
+
+export interface PizzaModel {
+  title: string;
+  description: string;
+  toppings: string[];
+  price: number;
+}
+
+export const Pizza = new DataStructure<PizzaModel>(
+  'https://localhost:3000/pizzas'
+);
